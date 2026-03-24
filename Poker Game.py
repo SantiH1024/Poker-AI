fa757d830be3d327a88182eaff20ac9b6a44fd52
SantiH1024 <156972703+SantiH1024@users.noreply.github.com>
2026-03-24 17:22:57 -0600

    funcs

---
+++ Poker Game.py
@@ -0,0 +1,43 @@
+#Game functions
+import random
+import math
+
+SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+RANKS = list(range(1, 14))
+
+def makeDeck():
+    deck = []
+
+    for suit in SUITS:
+        for rank in RANKS:
+            deck.append((rank, suit))
+
+    random.shuffle(deck)
+    return deck
+
+def playerPreFlopHand(deck):
+    card1 = deck.pop()
+    card2 = deck.pop()
+    player_pre_flop_hand = [card1,card2]
+    return player_pre_flop_hand
+
+def flop(deck):
+    card1 = deck.pop()
+    card2 = deck.pop()
+    card3 = deck.pop()
+    flop =  [card1,card2,card3]
+    return flop
+
+def fullHand(player_pre_flop_hand,flop):
+    full_hand = player_pre_flop_hand + flop
+    return full_hand
+
+
+
+def main():
+    deck = makeDeck()
+    hand = playerHand(deck)
+    print(hand)
+
+if __name__ == "__main__":
+    main()
