e1f9d3e9abcde804038fe494cb9260a329943549
ekweder <evkw2629@colorado.edu>
2026-03-24 21:32:26 -0600

    pokergame is now a class, main is now on main file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # start game
+
+from pokergame import Game
+
+def main():
+    print("Starting Poker Game...")
+    poker_game = Game()
+    poker_game.play()
+
+if __name__ == "__main__":
+    main()
--- pokergame.py
+++ pokergame.py
@@ -2,6 +2,7 @@
 #test
 import random
 import math
+from agents import Player
 
 SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
 RANKS = list(range(1, 14))
@@ -16,104 +17,105 @@ def makeDeck():
     random.shuffle(deck)
     return deck
 
-def playerPreFlopHand(deck):
-    card1 = deck.pop()
-    card2 = deck.pop()
-    player_pre_flop_hand = [card1,card2]
-    return player_pre_flop_hand
+class Game:
+    def __init__(self):
+        self.deck = makeDeck()
+        self.player1 = Player("Human", 1000)
+        self.player2 = Player("Bot", 1000)
+        self.flop_cards = []
+        self.pot = 0
 
-def flop(deck):
-    card1 = deck.pop()
-    card2 = deck.pop()
-    card3 = deck.pop()
-    flop =  [card1,card2,card3]
-    return flop
+    def playerPreFlopHand(self):
+        # gives player classes created by game preflop cards
+        self.player1.receive_card(self.deck.pop())
+        self.player1.receive_card(self.deck.pop())
+    
+        self.player2.receive_card(self.deck.pop())
+        self.player2.receive_card(self.deck.pop())
 
-def fullHand(player_pre_flop_hand,flop):
-    full_hand = player_pre_flop_hand + flop
-    return full_hand
+    def flop(self):
+        # draws 3 cards for the flop
+        self.flop_cards.append(self.deck.pop())
+        self.flop_cards.append(self.deck.pop())
+        self.flop_cards.append(self.deck.pop())
 
-def evaluateHand(full_hand):
+    def fullHand(self, player):
+        # gets cards in player hand and community flop cards
+        full_hand = player.hand + self.flop_cards
+        return full_hand
 
-    def isPair(full_hand):
-        ranks = []
+    def evaluateHand(self, full_hand):
+        def isPair(full_hand):
+            ranks = []
 
-        for card in full_hand:
-            ranks.append(card[0])
+            for card in full_hand:
+                ranks.append(card[0])
 
-        for rank in ranks:
-            if ranks.count(rank) == 2:
+            for rank in ranks:
+                if ranks.count(rank) == 2:
+                    return True
+
+            return False
+        
+        def isTwoPair(full_hand):
+            ranks = []
+
+            for card in full_hand:
+                ranks.append(card[0])
+
+            pair_count = 0
+
+            for rank in set(ranks):
+                if ranks.count(rank) == 2:
+                    pair_count += 1
+
+            return pair_count == 2
+        
+        def isThreeOfAKind(full_hand):
+            ranks = []
+
+            for card in full_hand:
+                ranks.append(card[0])
+
+            for rank in ranks:
+                if ranks.count(rank) == 3:
+                    return True
+                
+            return False
+        
+        def isFourOfAKind(full_hand):
+            ranks = []
+
+            for card in full_hand:
+                ranks.append(card[0])
+
+            for rank in ranks:
+                if ranks.count(rank) == 4:
+                    return True
+            return False
+        
+        def isStraight(full_hand):
+            ranks = []
+
+            for card in full_hand:
+                ranks.append(card[0])
+
+            ranks.sort()
+
+            if ranks == [1, 10, 11, 12, 13]:
                 return True
 
-        return False
-    
-    def isTwoPair(full_hand):
-        ranks = []
-
-        for card in full_hand:
-            ranks.append(card[0])
-
-        pair_count = 0
-
-        for rank in set(ranks):
-            if ranks.count(rank) == 2:
-                pair_count += 1
-
-        return pair_count == 2
-    
-    def isThreeOfAKind(full_hand):
-        ranks = []
-
-        for card in full_hand:
-            ranks.append(card[0])
-
-        for rank in ranks:
-            if ranks.count(rank) == 3:
-                return True
-            
-        return False
-    
-    def isFourOfAKind(full_hand):
-        ranks = []
-
-        for card in full_hand:
-            ranks.append(card[0])
-
-        for rank in ranks:
-            if rank.count(rank) == 4:
-                return True
-        return False
-    
-    def isStraight(full_hand):
-        ranks = []
-
-        for card in full_hand:
-            ranks.append(card[0])
-
-        ranks.sort()
-
-        if ranks == [1, 10, 11, 12, 13]:
+            if len(set(ranks)) != 5:
+                return False
+            for i in range(4):
+                if ranks[i + 1] != ranks[i] + 1:
+                    return False            
             return True
 
-        if len(set(ranks)) != 5:
-            return False
-        for i in range(4):
-            if ranks[i + 1] != ranks[i] + 1:
-                return False            
-        return True
+        def isFlush(full_hand):
+            suits = []
 
-    def isFlush(full_hand):
-        suits = []
+            for card in full_hand:
+                suits.append(card[1])
 
-        for card in full_hand:
-            suits.append(card[1])
-
-        return len(set(suits)) == 1
-
-def main():
-    deck = makeDeck()
-    hand = playerPreFlopHand(deck)
-    print(hand)
-
-if __name__ == "__main__":
-    main()
+            return len(set(suits)) == 1
